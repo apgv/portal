dc0b432555a51b6a06889b12e8b764a6e60c4aa5
apgv <ap@foobar.codes>
2017-11-06 13:35:06 +0100

    Removed initial Vue code. Added person registry table with test data

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,22 +1,29 @@
 <template>
     <div class="hello">
-        <h1>{{ msg }}</h1>
-        <h2>Essential Links</h2>
-        <ul>
-            <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-            <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-            <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-            <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-            <br>
-            <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-        </ul>
-        <h2>Ecosystem</h2>
-        <ul>
-            <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-            <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-            <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-            <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-        </ul>
+        <h1>Personregister</h1>
+
+        <table>
+            <thead>
+            <tr>
+                <td>ID</td>
+                <td>Fornavn</td>
+                <td>Etternavn</td>
+                <td>E-post</td>
+                <td>Telefon</td>
+                <td>Medlem</td>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="person in persons">
+                <td>{{person.id}}</td>
+                <td>{{person.firstName}}</td>
+                <td>{{person.lastName}}</td>
+                <td>{{person.email}}</td>
+                <td>{{person.phone}}</td>
+                <td>{{person.member ? 'Ja' : 'Nei'}}</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -25,7 +32,96 @@
         name: 'HelloWorld',
         data () {
             return {
-                msg: 'Welcome to Your Vue.js App'
+                persons: [
+                    {
+                        id: 1,
+                        firstName: 'Ben Redic',
+                        lastName: 'Fy Fazan',
+                        email: 'ben.redic.fy.fazan@example.com',
+                        phone: '41234567',
+                        member: false
+                    },
+                    {
+                        id: 2,
+                        firstName: 'Frimand',
+                        lastName: 'Pløsen',
+                        email: 'frimand.plosen@example.com',
+                        phone: '41234567',
+                        member: false
+                    },
+                    {
+                        id: 3,
+                        firstName: 'Hallstein',
+                        lastName: 'Bronskimlet d.a.y.',
+                        email: 'hallstein.bronskimlet.day@example.com',
+                        phone: '41234567',
+                        member: false
+                    },
+                    {
+                        id: 4,
+                        firstName: 'Ludvig',
+                        lastName: '',
+                        email: 'ludvig@example.com',
+                        phone: '41234567',
+                        member: false
+                    },
+                    {
+                        id: 5,
+                        firstName: 'Melvind',
+                        lastName: 'Snerken',
+                        email: 'melvind.snerken@example.com',
+                        phone: '41234567',
+                        member: false
+                    },
+                    {
+                        id: 6,
+                        firstName: 'Myrullbråten',
+                        lastName: '',
+                        email: 'myrullbraten@example.com',
+                        phone: '41234567',
+                        member: false
+                    },
+                    {
+                        id: 7,
+                        firstName: 'Mysil',
+                        lastName: 'Bergsprekken',
+                        email: 'mysil.bergsprekken@example.com',
+                        phone: '41234567',
+                        member: false
+                    },
+                    {
+                        id: 8,
+                        firstName: 'Reodor',
+                        lastName: 'Felgen',
+                        email: 'reodor.felgen@example.com',
+                        phone: '41234567',
+                        member: false
+                    },
+                    {
+                        id: 9,
+                        firstName: 'Rudolf',
+                        lastName: 'Blodstrupmoen',
+                        email: 'rudolf.blodstrupmoen@example.com',
+                        phone: '41234567',
+                        member: false
+                    },
+                    {
+                        id: 10,
+                        firstName: 'Sindre',
+                        lastName: 'Piltingsrud',
+                        email: 'sindre.piltingsrud@example.com',
+                        phone: '41234567',
+                        member: false
+                    },
+                    {
+                        id: 11,
+                        firstName: 'Solan',
+                        lastName: 'Gundersen',
+                        email: 'solan.gundersen@example.com',
+                        phone: '41234567',
+                        member: false
+
+                    }]
             }
         }
     }
@@ -49,5 +145,20 @@
 
     a {
         color: #42b983;
+    }
+
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    th, td {
+        padding: 8px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+    }
+
+    tr:hover {
+        background-color: #f5f5f5
     }
 </style>
