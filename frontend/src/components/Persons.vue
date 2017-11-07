4161409723a5eca5c0bd24edad9bd7f00e299796
apgv <ap@foobar.codes>
2017-11-07 22:20:48 +0100

    Replaced hard coded mock data with data from backend

--- frontend/src/components/Persons.vue
+++ frontend/src/components/Persons.vue
@@ -34,97 +34,20 @@
         props: ['authenticated'],
         data () {
             return {
-                persons: [
-                    {
-                        id: 1,
-                        firstName: 'Ben Redic',
-                        lastName: 'Fy Fazan',
-                        email: 'ben.redic.fy.fazan@example.com',
-                        phone: '41234567',
-                        member: false
-                    },
-                    {
-                        id: 2,
-                        firstName: 'Frimand',
-                        lastName: 'Pløsen',
-                        email: 'frimand.plosen@example.com',
-                        phone: '41234567',
-                        member: false
-                    },
-                    {
-                        id: 3,
-                        firstName: 'Hallstein',
-                        lastName: 'Bronskimlet d.a.y.',
-                        email: 'hallstein.bronskimlet.day@example.com',
-                        phone: '41234567',
-                        member: false
-                    },
-                    {
-                        id: 4,
-                        firstName: 'Ludvig',
-                        lastName: '',
-                        email: 'ludvig@example.com',
-                        phone: '41234567',
-                        member: false
-                    },
-                    {
-                        id: 5,
-                        firstName: 'Melvind',
-                        lastName: 'Snerken',
-                        email: 'melvind.snerken@example.com',
-                        phone: '41234567',
-                        member: false
-                    },
-                    {
-                        id: 6,
-                        firstName: 'Myrullbråten',
-                        lastName: '',
-                        email: 'myrullbraten@example.com',
-                        phone: '41234567',
-                        member: false
-                    },
-                    {
-                        id: 7,
-                        firstName: 'Mysil',
-                        lastName: 'Bergsprekken',
-                        email: 'mysil.bergsprekken@example.com',
-                        phone: '41234567',
-                        member: false
-                    },
-                    {
-                        id: 8,
-                        firstName: 'Reodor',
-                        lastName: 'Felgen',
-                        email: 'reodor.felgen@example.com',
-                        phone: '41234567',
-                        member: false
-                    },
-                    {
-                        id: 9,
-                        firstName: 'Rudolf',
-                        lastName: 'Blodstrupmoen',
-                        email: 'rudolf.blodstrupmoen@example.com',
-                        phone: '41234567',
-                        member: false
-                    },
-                    {
-                        id: 10,
-                        firstName: 'Sindre',
-                        lastName: 'Piltingsrud',
-                        email: 'sindre.piltingsrud@example.com',
-                        phone: '41234567',
-                        member: false
-                    },
-                    {
-                        id: 11,
-                        firstName: 'Solan',
-                        lastName: 'Gundersen',
-                        email: 'solan.gundersen@example.com',
-                        phone: '41234567',
-                        member: false
-
-                    }]
+                persons: []
             }
+        },
+        methods: {
+            fetchPersons () {
+                if (this.authenticated) {
+                    this.$http.get('api/persons').then(response => {
+                        this.persons = response.body
+                    })
+                }
+            }
+        },
+        created () {
+            this.fetchPersons()
         }
     }
 </script>
