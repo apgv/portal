0104eb4feae89326355adec2217b03ff1d841747
apgv <ap@foobar.codes>
2017-11-13 21:43:56 +0100

    Added modal registration form

--- frontend/src/components/Persons.vue
+++ frontend/src/components/Persons.vue
@@ -3,6 +3,102 @@
         <div v-if="authenticated">
             <h1 class="title">Personregister</h1>
 
+            <div class="modal"
+                 :class="{'is-active': showModal}">
+                <div class="modal-background">
+                </div>
+                <div class="modal-card">
+                    <header class="modal-card-head">
+                        <p class="modal-card-title">Registrer person</p>
+                        <button @click="toggleModal()"
+                                class="delete"
+                                aria-label="close"></button>
+                    </header>
+                    <section class="modal-card-body">
+                        <!-- Content ... -->
+                        <div class="field is-horizontal">
+                            <div class="field-label">
+                                <label class="label">Navn</label>
+                            </div>
+                            <div class="field-body">
+                                <div class="field">
+                                    <p class="control has-icons-left">
+                                        <input class="input" placeholder="Navn"/>
+                                        <span class="icon is-small is-left">
+                                    <i class="fa fa-user"></i>
+                                </span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="field is-horizontal">
+                            <div class="field-label">
+                                <label class="label">E-post</label>
+                            </div>
+                            <div class="field-body">
+                                <div class="field">
+                                    <p class="control has-icons-left">
+                                        <input class="input" type="email" placeholder="E-post"/>
+                                        <span class="icon is-small is-left">
+                                            <i class="fa fa-envelope"></i>
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="field is-horizontal">
+                            <div class="field-label">
+                                <label class="label">Telefon</label>
+                            </div>
+                            <div class="field-body">
+                                <div class="field">
+                                    <p class="control has-icons-left">
+                                        <input class="input" placeholder="Telefon"/>
+                                        <span class="icon is-small is-left">
+                                            <i class="fa fa-mobile"></i>
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="field is-horizontal">
+                            <div class="field-label">
+                                <label class="label">Medlemsskap</label>
+                            </div>
+                            <div class="field-body">
+                                <div class="field">
+                                    <div class="control">
+                                        <label class="checkbox">
+                                            <input type="checkbox" v-model="member"/>
+                                            Medlem
+                                        </label>
+                                    </div>
+                                </div>
+                                <div v-if="member"
+                                     class="field">
+                                    <p class="control">
+                                        <input class="input" placeholder="Årstall medlemsskap"/>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <footer class="modal-card-foot">
+                        <button @click="toggleModal()"
+                                class="button is-success">
+                            Lagre
+                        </button>
+                        <button @click="toggleModal()"
+                                class="button">
+                            Avbryt
+                        </button>
+                    </footer>
+                </div>
+            </div>
+            <button @click="toggleModal()"
+                    class="button">
+                Registrer
+            </button>
             <table class="table is-striped is-hoverable">
                 <thead>
                 <tr>
@@ -38,7 +134,9 @@
         props: ['auth', 'authenticated'],
         data () {
             return {
-                persons: []
+                persons: [],
+                showModal: true,
+                member: false
             }
         },
         methods: {
@@ -49,6 +147,9 @@
                         this.persons = response.body
                     })
                 }
+            },
+            toggleModal () {
+                this.showModal = !this.showModal
             }
         },
         created () {
