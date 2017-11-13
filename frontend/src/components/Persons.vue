0d6fd07c19a2650279b6155f18242d1958b49daf
apgv <ap@foobar.codes>
2017-11-13 13:47:22 +0100

    Added link to login

--- frontend/src/components/Persons.vue
+++ frontend/src/components/Persons.vue
@@ -1,36 +1,41 @@
 <template>
-    <div v-if="authenticated">
-        <h1 class="title">Personregister</h1>
+    <div>
+        <div v-if="authenticated">
+            <h1 class="title">Personregister</h1>
 
-        <table class="table is-striped is-hoverable">
-            <thead>
-            <tr>
-                <td>ID</td>
-                <td>Fornavn</td>
-                <td>Etternavn</td>
-                <td>E-post</td>
-                <td>Telefon</td>
-                <td>Medlem</td>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="person in persons">
-                <td>{{person.id}}</td>
-                <td>{{person.firstName}}</td>
-                <td>{{person.lastName}}</td>
-                <td>{{person.email}}</td>
-                <td>{{person.phone}}</td>
-                <td>{{person.member ? 'Ja' : 'Nei'}}</td>
-            </tr>
-            </tbody>
-        </table>
+            <table class="table is-striped is-hoverable">
+                <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Fornavn</td>
+                    <td>Etternavn</td>
+                    <td>E-post</td>
+                    <td>Telefon</td>
+                    <td>Medlem</td>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="person in persons">
+                    <td>{{person.id}}</td>
+                    <td>{{person.firstName}}</td>
+                    <td>{{person.lastName}}</td>
+                    <td>{{person.email}}</td>
+                    <td>{{person.phone}}</td>
+                    <td>{{person.member ? 'Ja' : 'Nei'}}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div v-if="!authenticated">
+            Du er ikke logget inn. Vennligst <a @click="auth.login()">logg inn</a> for å fortsette.
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         name: 'Persons',
-        props: ['authenticated'],
+        props: ['auth', 'authenticated'],
         data () {
             return {
                 persons: []
