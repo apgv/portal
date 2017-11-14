5f18885c7b6b3eb7e97da0626c143c6ba8c9d9ae
apgv <ap@foobar.codes>
2017-11-14 19:03:56 +0100

    Added vee-validate to do form validation and Norwegian as locale

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import VueResource from 'vue-resource'
+import nbNO from 'vee-validate/dist/locale/nb_NO'
+import VeeValidate, {Validator} from 'vee-validate'
 import Home from '@/components/Home'
 import Persons from '@/components/Persons'
 import Callback from '@/components/Callback'
 
 Vue.use(Router)
 Vue.use(VueResource)
+Vue.use(VeeValidate)
+
+Validator.localize('nb_NO', nbNO)
 
 const router = new Router({
     mode: 'history',
